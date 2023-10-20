61e1ac3a46d84e499098395723f7d507c1cc33a6
mmusgrave <mmusgrave@pobox.com>
2023-10-20 19:02:09 +0200

    Change all timezones to EST

--- gamedaybot/espn/scheduler.py
+++ gamedaybot/espn/scheduler.py
@@ -16,11 +16,10 @@ def scheduler():
     None
     """
     data = get_env_vars()
-    game_timezone = 'America/New_York'
+    eastern_standard_timezone = 'America/New_York'
     sched = BlockingScheduler(job_defaults={'misfire_grace_time': 15 * 60})
     ff_start_date = data['ff_start_date']
     ff_end_date = data['ff_end_date']
-    my_timezone = data['my_timezone']
 
     # close scores (within 15.99 points): monday evening at 6:30pm east coast time.
     # power rankings:                     tuesday evening at 6:30pm local time.
@@ -34,41 +33,41 @@ def scheduler():
 
     sched.add_job(espn_bot, 'cron', ['get_close_scores'], id='close_scores',
                   day_of_week='mon', hour=18, minute=30, start_date=ff_start_date, end_date=ff_end_date,
-                  timezone=game_timezone, replace_existing=True)
+                  timezone=eastern_standard_timezone, replace_existing=True)
     sched.add_job(espn_bot, 'cron', ['get_power_rankings'], id='power_rankings',
                   day_of_week='tue', hour=18, minute=30, start_date=ff_start_date, end_date=ff_end_date,
-                  timezone=my_timezone, replace_existing=True)
+                  timezone=eastern_standard_timezone, replace_existing=True)
     sched.add_job(espn_bot, 'cron', ['get_final'], id='final',
                   day_of_week='tue', hour=7, minute=30, start_date=ff_start_date, end_date=ff_end_date,
-                  timezone=my_timezone, replace_existing=True)
+                  timezone=eastern_standard_timezone, replace_existing=True)
     sched.add_job(espn_bot, 'cron', ['get_standings'], id='standings',
                   day_of_week='wed', hour=7, minute=30, start_date=ff_start_date, end_date=ff_end_date,
-                  timezone=my_timezone, replace_existing=True)
+                  timezone=eastern_standard_timezone, replace_existing=True)
     sched.add_job(espn_bot, 'cron', ['get_waiver_report'], id='waiver_report',
                   day_of_week='wed', hour=7, minute=31, start_date=ff_start_date, end_date=ff_end_date,
-                  timezone=my_timezone, replace_existing=True)
+                  timezone=eastern_standard_timezone, replace_existing=True)
 
     if data['daily_waiver']:
         sched.add_job(
             espn_bot, 'cron', ['get_waiver_report'],
             id='waiver_report', day_of_week='mon, tue, thu, fri, sat, sun', hour=7, minute=31, start_date=ff_start_date,
-            end_date=ff_end_date, timezone=my_timezone, replace_existing=True)
+            end_date=ff_end_date, timezone=eastern_standard_timezone, replace_existing=True)
 
     sched.add_job(espn_bot, 'cron', ['get_matchups'], id='matchups',
                   day_of_week='thu', hour=19, minute=30, start_date=ff_start_date, end_date=ff_end_date,
-                  timezone=game_timezone, replace_existing=True)
+                  timezone=eastern_standard_timezone, replace_existing=True)
     sched.add_job(espn_bot, 'cron', ['get_scoreboard_short'], id='scoreboard1',
                   day_of_week='fri,mon', hour=7, minute=30, start_date=ff_start_date, end_date=ff_end_date,
-                  timezone=my_timezone, replace_existing=True)
+                  timezone=eastern_standard_timezone, replace_existing=True)
 
     if data['monitor_report']:
         sched.add_job(espn_bot, 'cron', ['get_monitor'], id='monitor',
                       day_of_week='sun', hour=7, minute=30, start_date=ff_start_date, end_date=ff_end_date,
-                      timezone=my_timezone, replace_existing=True)
+                      timezone=eastern_standard_timezone, replace_existing=True)
 
     sched.add_job(espn_bot, 'cron', ['get_scoreboard_short'], id='scoreboard2',
                   day_of_week='sun', hour='16,20', start_date=ff_start_date, end_date=ff_end_date,
-                  timezone=game_timezone, replace_existing=True)
+                  timezone=eastern_standard_timezone, replace_existing=True)
 
     print("Ready!")
     sched.start()
